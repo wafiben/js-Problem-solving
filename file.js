fb23bd91f02db684dc0d45fc8026636dcc476a51
wafiben <wafibenjeddou@gmail.com>
2022-10-05 01:38:18 +0200

    problem-7

--- file.js
+++ file.js
@@ -23,14 +23,52 @@ function factorial(n) {
 }
 /* problem 4:Return the length of the longest word in the provided sentence. */
 function lengthOfWord(str) {
-  let tableo = [];
+  let table = str.split(" ").map((elt) => elt.length);
+  return Math.max(...table);
+}
+/* proble 4:Return Largest Numbers in Arrays from subsarrays */
+function largestOfFour(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = Math.max(...arr[i]);
+  }
+  return arr;
+}
+/* problem 5:Check if a string (first argument, str) ends with the given target string (second argument, target). */
+/* example */ /* confirmEnding("Bastian", "n") should return true */
+/* confirmEnding("Congratulation", "on") should return true. */
+function confirmEnding(str, target) {
   let STR = "";
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 1; j < str.length; j++) {
-      if (str[j] == " ") {
-        STR = table.push(str);
-      }
-    }
+  for (let i = str.length - target.length; i < str.length; i++) {
+    STR = STR + str[i];
+  }
+  if (STR === target) {
+    return true;
+  } else {
+    return false;
   }
 }
-
+/* Problem 6:Repeat a given string str (first argument) for num times (second argument) */
+/* repeatStringNumTimes("*", 3) should return the string *** */
+function repeatStringNumTimes(str, num) {
+  let STR = "";
+  for (let i = 0; i < num; i++) {
+    STR = STR + str;
+  }
+  return STR;
+}
+/* Problem 7:Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending. */
+/* truncateString("A-tisket a-tasket A green and yellow basket", 8) should return the string A-tisket.... */
+/* "A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length) should return the string A-tisket a-tasket A green and yellow basket */
+function truncateString(str, num) {
+  let i = 0;
+  let STR = "";
+  while (i < num) {
+    STR = STR + str[i];
+    i++;
+  }
+  if (str.length > num) {
+    return STR + "...";
+  } else {
+    return str;
+  }
+}
